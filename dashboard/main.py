254fb4f8505d6a6bd0cdaf9e7b4ccc2b7e0c032b
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-06 16:30:54 +0100

    dash example plot

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,20 +1,27 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+user1 = pd.read_csv("../kacper.csv")
 
 app.layout = html.Div([
     dcc.Markdown(children="# TWD projekt 3 dashboard"),
     html.Label("Siema"),
     dcc.RadioItems(options=[
-        {"label": "No siema", "value": "nsiema"},
-        {"label": "Elo", "value": "elo"},
-        {"label": "Witam", "value": "w"}
-    ])
+        {"label": "Jakub", "value": "user1"},
+        {"label": "Jan", "value": "user2"},
+        {"label": "Kacper", "value": "user3"}
+    ]),
+    dcc.Graph(
+        id="main",
+        figure=px.histogram(user1, x="domain")
+    )
 
 ])
 
