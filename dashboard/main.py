e6e8dbd2caa0072580abadab451654e50976fb9b
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-09 17:02:54 +0100

    dość

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import plotly.express as px
 from dash.dependencies import Input, Output
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -24,24 +23,50 @@ DOMAINS = {"Stackoverflow": "stackoverflow.com",
            "Instagram": "instagram.com"}
 
 
-def to_dashformat(map: dict):
+def to_dashformat(mapa: dict):
     out = []
-    for key in map:
-        out.append({"label": key, "value": map[key]})
+    for key in mapa:
+        out.append({"label": key, "value": mapa[key]})
     return out
 
 
 app.layout = html.Div([
     dcc.Markdown(children="# TWD projekt 3 dashboard"),
-    html.Label("Siema"),
-    dcc.RadioItems(
-        id="user-select",
-        options=[
-            {"label": "Jakub", "value": "user1"},
-            {"label": "Jan", "value": "user2"},
-            {"label": "Kacper", "value": "user3"}
-        ]
+
+    dcc.Markdown(children="## Total usage over time"),
+    dcc.Graph(
+        id="TotalLineplot",
+        figure=px.histogram(user1, x="domain")  # todo: wygenerować lineplot
     ),
+    html.Label("Choose which sites to sum"),
+    dcc.Dropdown(
+        options=to_dashformat(DOMAINS),
+        multi=True,
+        value=[i for i in DOMAINS.values()]
+    ),
+
+    dcc.Markdown(children="## Per weekday/hour usage"),
+    html.Div([
+        html.Div([
+            html.Label("Choose person"),
+            dcc.RadioItems(
+                id="user-select",
+                options=[
+                    {"label": "Jakub", "value": "user1"},
+                    {"label": "Jan", "value": "user2"},
+                    {"label": "Kacper", "value": "user3"}
+                ]
+            ),
+            html.Label("test tttttttttttt ttttttttttt tttttttttttt")  # todo: this shit dont work
+        ], style={"width": "16rem", "height": "100%"}),
+        html.Br(),
+        dcc.Graph(
+            id="PerDayBarplot",
+            figure=px.histogram(user1, x="domain")  # todo: wygenerować plot
+        )
+    ], style={"columnCount": 2}),
+
+    # zostawiam ten bałagan na razie
     html.Label('Domeny'),
     dcc.Checklist(
         id="domain-select",
@@ -51,19 +76,7 @@ app.layout = html.Div([
     dcc.Graph(
         id="histogram",
         figure=px.histogram(user1, x="domain")
-    ),
-    dcc.Markdown(id="text",
-    children="""
-    test test test
-    
-    test 
-    # test
-    ## test
-    ### test
-    ```
-    test
-    ```
-    """),
+    )
 ])
 
 
