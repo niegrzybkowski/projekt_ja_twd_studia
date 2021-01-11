7977a0ed033aedbb09d744b251886c8cd5da080e
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-11 18:35:28 +0100

    spooky skeleton

--- dashboard/server.R
+++ dashboard/server.R
@@ -2,40 +2,13 @@ library(DT)
 library(ggplot2)
 
 function(input, output, session){
-  
-
-  
-  output$table_1 <- DT::renderDataTable({
-    DT::datatable(iris)
-  })
-  
   output$plot_1 <- renderPlot({
-    sample_row <- sample(1:nrow(iris), input$plot_1_sample)
-    sampled_iris <- iris[sample_row, ]
-    
-    if (input$plot_1_colors) {
-      ggplot(sampled_iris, aes_string(x = input$plot_1_x, y = input$plot_1_y, color = "Species")) +
-        geom_point()  
-    } else {
-      ggplot(sampled_iris, aes_string(x = input$plot_1_x, y = input$plot_1_y)) +
-        geom_point()
-    }
-    
+
   })
-  
-  output$plotly_1 <- renderPlotly({
-    sample_row <- sample(1:nrow(iris), input$plot_1_sample)
-    sampled_iris <- iris[sample_row, ]
-    
-    if (input$plot_1_colors) {
-      p <- ggplot(sampled_iris, aes_string(x = input$plot_1_x, y = input$plot_1_y, color = "Species")) +
-        geom_point()  
-    } else {
-      p <- ggplot(sampled_iris, aes_string(x = input$plot_1_x, y = input$plot_1_y)) +
-        geom_point()
-    }
-    ggplotly(p)
-    
+  output$plot_2 <- renderPlot({
+
   })
-  
+  output$plot_3 <- renderPlot({
+
+  })
 }
