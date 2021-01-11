5cf426a0788ea735a7a453d3c99e8e4243d56be2
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-11 18:36:19 +0100

    kn;l

--- dashboard/server.R
+++ dashboard/server.R
@@ -3,12 +3,12 @@ library(ggplot2)
 
 function(input, output, session){
   output$plot_1 <- renderPlot({
-
+    # cały okres
   })
   output$plot_2 <- renderPlot({
-
+    # dzień tygodnia
   })
   output$plot_3 <- renderPlot({
-
+    # godzina
   })
 }
