4da428dbf7beb84e0b3cf9d1a6053df8b1c46098
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2021-01-11 20:58:34 +0100

    weekdays

--- dashboard/server.R
+++ dashboard/server.R
@@ -5,7 +5,7 @@ function(input, output, session){
   output$plot_1 <- renderPlot({
     # cały okres
     read.csv("../data/allCount.csv") %>%
-      as_tibble() %>%
+      dplyr::as_tibble() %>%
       mutate(date = as.Date(date)) %>%
       group_by(date, user, domain) %>%
       summarise(count = sum(count)) %>%
@@ -17,7 +17,11 @@ function(input, output, session){
       scale_y_continuous(limits = c(0, NA))
   })
   output$plot_2 <- renderPlot({
-    # dzień tygodnia
+    read.csv("../data/avgweekdays.csv") %>%
+      filter(user == input$user_select, domain == input$domain_select) %>%
+    ggplot(aes(x = weekday, y = average)) +
+      geom_col() + theme_bw()
+
   })
   output$plot_3 <- renderPlot({
     # godzina
