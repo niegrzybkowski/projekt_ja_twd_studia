fdbe75da74aca6987142cbc5c4922b49673c6f03
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-13 14:43:32 +0100

    add colors df

---
+++ colors.R
@@ -0,0 +1,38 @@
+colors_df <- tibble(
+  domain = c(
+    "stackoverflow.com",
+    "wikipedia.org",
+    "github.com",
+    "pw.edu.pl",
+    "youtube.com",
+    "google.com",
+    "facebook.com",
+    "instagram.com"),
+  light_color = c(
+    "#f1b42b",
+    "#aaaaaa",
+    "#a84cb3",
+    "#4fc9bd",
+    "#ff2000",
+    "#cbdeef",
+    "#5198f5",
+    "#b130ba"),
+  medium_color = c(
+    "#f48024",
+    "#777777",
+    "#651678",
+    "#0095af",
+    "#dd0000",
+    "#8cb9e0",
+    "#1877f2",
+    "#4d5eca"),
+  dark_color = c(
+    "#c25e25",
+    "#333333",
+    "#482577",
+    "#1c475a",
+    "#aa0000",
+    "#5d87af",
+    "#0b5ac1",
+    "#e8c04a")
+)
