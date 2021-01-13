e68af4f6deb11a28a6ccea73c801006800baf1cc
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-13 15:47:11 +0100

    color test

--- dashboard/server.R
+++ dashboard/server.R
@@ -5,6 +5,44 @@ library(lubridate)
 library(dplyr)
 
 function(input, output, session){
+  colors_df <- tibble(
+    domain = c(
+      "stackoverflow.com",
+      "wikipedia.org",
+      "github.com",
+      "pw.edu.pl",
+      "youtube.com",
+      "google.com",
+      "facebook.com",
+      "instagram.com"),
+    light_color = c(
+      "#ffc43b",
+      "#cccccc",
+      "#e88cd3",
+      "#5fd9cd",
+      "#ff7777",
+      "#cbdeef",
+      "#91c8ff",
+      "#b130ba"),
+    medium_color = c(
+      "#f48024",
+      "#777777",
+      "#a526b8",
+      "#0095af",
+      "#dd3333",
+      "#8cb9e0",
+      "#1877f2",
+      "#4d5eca"),
+    dark_color = c(
+      "#b24e15",
+      "#222222",
+      "#381567",
+      "#1c475a",
+      "#990000",
+      "#5d87af",
+      "#0b5ac1",
+      "#e8c04a")
+  )
   output$plot_1 <- renderPlot({
     # cały okres
     read.csv("../data/allCount.csv") %>%
@@ -13,11 +51,14 @@ function(input, output, session){
       group_by(date, user, domain) %>%
       summarise(count = sum(count)) %>%
       filter(domain == input$domain_select) %>%
-      ggplot(aes(x = date, y = count, color = user)) +
-      geom_smooth(se = F) +
+    ggplot(aes(x = date, y = count, color = user)) +
+      geom_smooth(se = F, formula = y ~ x, method = "loess", family = "quasipoisson") +
       theme_bw() +
       scale_x_date(limits = as.Date(c("2020-01-01", "2021-02-01"))) +
-      scale_y_continuous(limits = c(0, NA))
+      scale_y_continuous(limits = c(0, NA)) +
+      scale_color_manual(
+        values = colors_df %>% filter(domain == input$domain_select) %>% select(-domain) %>% unlist(use.names = F)
+      )
   })
   output$plot_2 <- renderPlot({
     read.csv("../data/avgweekdays.csv") %>%
