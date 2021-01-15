df8ceb0fc43ae2b6dac2560d8d1077c09e8bedd3
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2021-01-15 17:34:00 +0100

    move generation script

--- dashboard/server.R
+++ dashboard/server.R
@@ -178,16 +178,6 @@ function(input, output, session){
   })
 
   #STRONA 3
-  generate_csv <- function() {
-    all_c %>%
-      mutate(category = ifelse(domain %in% c("stackoverflow.com", "wikipedia.org", "github.com","pw.edu.pl"), "edu", "ent")) %>%
-      mutate(date = as.Date(paste0(lubridate::year(date), "-", lubridate::month(date), "-01"))) %>%
-      group_by(user, date, category) %>%
-      summarise(avg = mean(count)) %>%
-      tidyr::pivot_wider(names_from = category, values_from = avg) %>%
-      write.csv("data/balance.csv", row.names = F)
-  }
-
   balance_df <- read.csv("data/balance.csv", stringsAsFactors = F)
 
   output$plot_scatter <- renderPlot({
